db1aec755425ff2ceb500a1dc8aa84be3147bc33
Alastair Crabtree <alastaircrabtree@gmail.com>
2016-01-13 00:23:28 +0000

    Add the bowling game kata in javascript using jasmine

---
+++ JsKata/app.js
@@ -0,0 +1,57 @@
+﻿
+function Game() {
+    var rolls = [];
+    this.roll = function (pins) {
+        rolls.push(pins);
+    };
+
+    this.score = function () {
+        var total = 0;
+        var firstRollIndexPerFrame = 0;
+
+        for (var frameIndex = 0; frameIndex < 10; frameIndex++) {
+
+            total += firstRoll(firstRollIndexPerFrame);
+
+            if (isAStrike(firstRollIndexPerFrame)) {
+                total += strikeBonus(firstRollIndexPerFrame);
+                firstRollIndexPerFrame++;
+                continue;
+            }
+
+            total += secondRoll(firstRollIndexPerFrame);
+
+            if (isASpare(firstRollIndexPerFrame)) {
+                total += spareBonus(firstRollIndexPerFrame);
+            }
+
+            firstRollIndexPerFrame += 2;
+        }
+       
+        return total;
+    };
+
+    function firstRoll(rollIndex) {
+        return rolls[rollIndex];
+    }
+
+    function secondRoll(rollIndex) {
+        return rolls[rollIndex+1];
+    }
+
+     function spareBonus(rollIndex) {
+         return rolls[rollIndex + 2];
+     }
+
+     function strikeBonus(rollIndex) {
+         return rolls[rollIndex + 1] + rolls[rollIndex + 2];
+     }
+
+    function isASpare(rollIndex) {
+        return firstRoll(rollIndex) + secondRoll(rollIndex) === 10;
+    }
+
+    function isAStrike(rollIndex) {
+        return firstRoll(rollIndex) === 10;
+    }
+}
---
+++ JsKata/test.js
@@ -0,0 +1,60 @@
+﻿/// <reference path="Scripts/_references.js" />
+
+describe("my bowling score calculator", function () {
+    var game;
+
+    beforeEach(function() {
+        game = new Game();
+    });
+
+    it("should score zero when i miss every pin", function() {
+        rollBalls(game, 0, 20);
+        expect(game.score()).toBe(0);
+    });
+
+    it("returns 20 if a 1 has been rolled on every throw", function() {
+        rollBalls(game, 1, 20);
+        expect(game.score()).toBe(20);
+    });
+
+    it("rolling one spare adds the bonus from the next roll to the score", function () {
+
+        game.roll(5);
+        game.roll(5);
+
+        rollBalls(game, 1, 18);
+
+        expect(game.score()).toBe(29);
+    });
+
+    it("doesnt add bonus from prev roll if it wasnt a spare", function() {
+
+        game.roll(5);
+        game.roll(4);
+        game.roll(6);
+        game.roll(1);
+
+        rollBalls(game, 1, 16);
+
+        expect(game.score()).toBe(32);
+    });
+
+    it("rolling one strike adds the bonus from the next 2 rolls to the score", function () {
+        game.roll(10);
+
+        rollBalls(game, 1, 18);
+
+        expect(game.score()).toBe(30);
+    });
+
+    it("should score 300 in a pefect game", function () {
+        rollBalls(game, 10, 12);
+        expect(game.score()).toBe(300);
+    });
+
+    function rollBalls(game, score, times) {
+        for (var throwIndex = 0; throwIndex < times; throwIndex++) {
+            game.roll(score);
+        }
+    }
+}); 
